bf2913ad79d73c287006d421a708138bf1689c00
PuddingPudding <hwsh70119@gmail.com>
2018-05-18 00:23:49 +0800

    基本連線功能與是否為server的簡單測試，另外也更新gitignore避免其他版本Unity衝突

---
+++ Assets/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class Movement : NetworkBehaviour
+{
+    [SerializeField] private float m_fSpeed = 5;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (hasAuthority) //hasAuthority代表玩家是否有控制權
+        //{
+        //    float x = Input.GetAxis("Horizontal");
+        //    float y = Input.GetAxis("Vertical");
+
+        //    transform.Translate(new Vector2(x, y) * Time.deltaTime * m_fSpeed);
+        //}
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(x, y) * Time.deltaTime * m_fSpeed);
+    }
+}
---
+++ Assets/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class Unit : NetworkBehaviour
+{
+    public float maxHP = 50;
+    public Text uiText;
+    [SyncVar]
+    private float hp;
+
+    void Start()
+    {
+        hp = maxHP;
+    }
+
+    void Update()
+    {
+        if (isServer)
+        {
+            if (hp < maxHP)
+            {
+                hp += Time.deltaTime * 5;
+            }
+            else
+            {
+                hp = maxHP;
+            }
+        }
+        uiText.text = "HP:" + ((int)hp).ToString();
+    }
+    void OnMouseDown()
+    {
+        hp = 0;
+    }
+}
